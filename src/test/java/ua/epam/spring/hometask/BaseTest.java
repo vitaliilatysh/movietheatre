c5ad42380e5e5621b0a9097e05e0ce474fb42308
Vitalii Latysh <vitalii.latysh@globallogic.com>
2020-02-08 18:03:00 +0200

    Renamed config context files

--- src/test/java/ua/epam/spring/hometask/BaseTest.java
+++ src/test/java/ua/epam/spring/hometask/BaseTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ua.epam.spring.hometask.service.DiscountService;
 
 @RunWith(JUnit4.class)
@@ -13,7 +13,7 @@ public class BaseTest {
     @Test
     public void test() {
         ApplicationContext context =
-                new FileSystemXmlApplicationContext("/src/main/resources/beans.xml", "/src/main/resources/discount-strategies-beans.xml");
+                new ClassPathXmlApplicationContext("classpath*:application-context.xml", "classpath*:strategies-context.xml");
         DiscountService discountService = (DiscountService) context.getBean("discountService");
     }
 }
