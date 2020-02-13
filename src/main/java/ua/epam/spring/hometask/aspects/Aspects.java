9badd9403c9437b5855f476a441c5d4eded36511
Vitalii_Latysh <vitalii_latysh@epam.com>
2020-02-13 11:32:50 +0200

    Added draft aspect for getTicketPrice method from BookingService class

--- src/main/java/ua/epam/spring/hometask/aspects/Aspects.java
+++ src/main/java/ua/epam/spring/hometask/aspects/Aspects.java
@@ -1,8 +1,22 @@
 package ua.epam.spring.hometask.aspects;
 
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class Aspects {
+
+    @Pointcut("execution(* ua.epam.spring.hometask.service.impl.BookingServiceImpl.getTicketsPrice(..))")
+    public void getTicketPrice() {
+
+    }
+
+    @After("getTicketPrice()")
+    public void counterTicketPriceCalling() {
+        System.out.println("getTicketsPrice() called");
+    }
 }
--- src/main/java/ua/epam/spring/hometask/config/AppConfig.java
+++ src/main/java/ua/epam/spring/hometask/config/AppConfig.java
@@ -3,10 +3,12 @@ package ua.epam.spring.hometask.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan("ua.epam.spring.hometask.*")
 @PropertySource("file:src/main/resources/auditorium.properties")
 public class AppConfig {
